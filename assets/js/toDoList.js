79dc9530e080ca99406a9069b60d226681a806fb
Berar Iacob <contactiacob@gmail.com>
2017-12-18 00:36:05 +0200

    deleteCompletedTodo

--- assets/js/toDoList.js
+++ assets/js/toDoList.js
@@ -1,3 +1,13 @@
-$('li').on('click', function(){
-    $(this).toggleClass('completed');
+// Marking completion
+$("li").on("click", function(){
+    $(this).toggleClass("completed");
+});
+
+// Delete completed toDo
+
+$("span").on("click", function(event){
+    event.stopPropagation();
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
 });
