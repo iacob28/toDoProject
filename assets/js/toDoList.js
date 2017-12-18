08fdd1376f28203f0dd44fd4f49e49e01f527e1a
Iacob Berar <iacob.berar@intern.arobs.com>
2017-12-18 12:46:36 +0200

    beautifyFile

--- assets/js/toDoList.js
+++ assets/js/toDoList.js
@@ -1,21 +1,20 @@
 // Marking completion
-$("ul").on("click", "li", function(){
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
 });
 
 // Delete completed toDo
-
-$("ul").on("click", "span", function(event){
+$("ul").on("click", "span", function (event) {
     event.stopPropagation();
-    $(this).parent().fadeOut(500, function(){
+    $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
 });
 
 // Add toDo to list
-$("input[type='text']").keypress(function(event){
-    if(event.which === 13){
-        var inputText = $(this).val();        
+$("input[type='text']").keypress(function (event) {
+    if (event.which === 13) {
+        var inputText = $(this).val();
         $("ul").append("<li><span>X</span> " + inputText + "</li>");
         $(this).val("");
     }
