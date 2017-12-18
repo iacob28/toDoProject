0cc14f74bfd9d249ff73705d425caf9a5d18346b
Iacob Berar <iacob.berar@intern.arobs.com>
2017-12-18 12:18:51 +0200

    Add toDo to List

--- assets/js/toDoList.js
+++ assets/js/toDoList.js
@@ -1,13 +1,22 @@
 // Marking completion
-$("li").on("click", function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
 // Delete completed toDo
 
-$("span").on("click", function(event){
+$("ul").on("click", "span", function(event){
     event.stopPropagation();
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
+});
+
+// Add toDo to list
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var inputText = $(this).val();        
+        $("ul").append("<li><span>X</span> " + inputText + "</li>");
+        $(this).val("");
+    }
 });
